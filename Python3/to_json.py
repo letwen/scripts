a3ce4269c418d693245a2c498b08d4633653d596
letwen <letwen@foxmail.com>
2019-06-25 16:20:32 +0800

    python3 first upload

---
+++ Python3/to_json.py
@@ -0,0 +1,28 @@
+def tf_json(j_name,info,dsp="no") :
+    import os
+    import json
+    path=os.path.abspath(os.path.curdir)
+    j_path=path+"\\json"
+    j_file=j_path+f"\\{j_name}"
+    if os.path.isdir(j_path) :
+        print("json folder already exists")
+    else :
+        try :
+            os.makedirs(j_path)
+        except :
+            return 'failed to create json folder'
+        else :
+            print("successful creation of json folder")
+    try :
+        j_file_open=open(j_file,mode='w+')
+        json.dump(info,j_file_open)
+    except :
+        return 'Error, failed to create or write file'
+    else :
+        print("Write file successfully")
+    finally :
+        j_file_open.close()
+        if dsp == "yes" :
+            return json.dumps(info)
+        else :
+            return 'no display'
